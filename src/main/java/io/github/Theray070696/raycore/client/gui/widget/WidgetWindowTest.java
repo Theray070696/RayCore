35e267272527ee72578a9f78ba164b0e158387e0
Theray070696 <Theray070696@gmail.com>
2017-08-01 16:16:33 -0400

    Add text to test window

--- src/main/java/io/github/Theray070696/raycore/client/gui/widget/WidgetWindowTest.java
+++ src/main/java/io/github/Theray070696/raycore/client/gui/widget/WidgetWindowTest.java
@@ -1,6 +1,7 @@
 package io.github.Theray070696.raycore.client.gui.widget;
 
 import io.github.Theray070696.raycore.client.gui.widget.window.WindowButtonToggle;
+import io.github.Theray070696.raycore.client.gui.widget.window.WindowText;
 
 /**
  * Created by Theray070696 on 8/1/2017.
@@ -16,5 +17,6 @@ public class WidgetWindowTest extends WidgetWindow
     public void initializeFeatures()
     {
         this.addFeature(new WindowButtonToggle(50, 17, 78, 26));
+        this.addFeature(new WindowText(4, 17, "Test Button", 0x00bf00));
     }
 }
